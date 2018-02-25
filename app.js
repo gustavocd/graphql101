af25586732ee3ef6421012fff1e0ebf24fb31520
Gustavo Castillo <gcdcoder@gmail.com>
2018-02-24 21:22:31 -0600

    Create server and enable graphql in express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true,
+}));
+
+app.listen(3000, () => {
+  console.log('Server listen on port 3000');
+});
